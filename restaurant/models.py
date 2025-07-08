726420bcff91abea4c996c6d27a7a55fd17b4b6b
Nyasha Nallen Tumba <nyashatumba@gmail.com>
2025-07-08 14:09:27 +0200

    Models Creation

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
+
+    class Meta:
+        db_table = 'booking'
+    
+    def __str__(self):
+        return self.name
+    
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+    class Meta:
+        db_table = 'menu'
+
+    def __str__(self):
+        return self.title
+    
+    
