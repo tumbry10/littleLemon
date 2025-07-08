60ffcdeb10dec52e52a71f74fba11a16bdd8dded
Nyasha Nallen Tumba <nyashatumba@gmail.com>
2025-07-08 11:21:32 +0200

    MySQL database configuration

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littleLemon',
+        'USER' : 'root',
+        'PASSWORD' : 'N@ll$n17',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+        'OPTIONS' : {
+            'init_command' : "SET sql_mode = 'STRICT_TRANS_TABLES'"
+        }
     }
 }
 
@@ -117,10 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
